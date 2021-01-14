1dc497056dc88cb9fa369fb67e2aceb42bc050d9
johnnyyvard <johnnyyvard@me.com>
2021-01-14 21:44:38 +0100

    Évocation de npm

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // console.log(process.env.NODE_ENV);
 // process.env.NODE_ENV = 'production';
-// console.log(process.env.NODE_ENV);
 const name = require('./module');
 const http = require('http');
 
-console.log('bonjour' + name);
+console.log(process.env.NODE_ENV);
+require('dotenv').config();
+console.log(process.env.NODE_ENV);
+
+// console.log('bonjour' + name);
