a5d16600098d022d8ac5af4b43db24d9cef0bbeb
johnnyyvard <johnnyyvard@me.com>
2021-01-16 20:55:33 +0100

    Utilisation d'un stream existant

---
+++ streams/streams.js
@@ -0,0 +1,7 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on("data", evaluate);
+process.stdin.on("pause", () => console.log('Terminé.'));
+
+function evaluate(input) {
+    input.includes('pause') ? process.stdin.pause(): console.log(input);
+}
