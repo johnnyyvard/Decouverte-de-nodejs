332dff8b12be12d821997b1f86cdb5a51212ef3d
johnnyyvard <johnnyyvard@me.com>
2021-01-16 07:42:14 +0100

    Lecture et écriture d'un fichier

--- async-io.js
+++ async-io.js
@@ -1,26 +1,44 @@
 // Sync
-function doSomething_sync() {
-    let data = getUserDetails_sync();
-    console.log(data);
-    console.log('Disponible');
-}
+// function doSomething_sync() {
+//     let data = getUserDetails_sync();
+//     console.log(data);
+//     console.log('Disponible');
+// }
 
-function getUserDetails_sync() {
-    const timout = Date.now() + 2000;
-    while (Date.now() < timout) { };
-    return 'Elise';
-}
+// function getUserDetails_sync() {
+//     const timout = Date.now() + 2000;
+//     while (Date.now() < timout) { };
+//     return 'Elise';
+// }
 
-doSomething_sync();
+// doSomething_sync();
 
 // Async
-function doSomething_async() {
-    getUserDetails_async((data) => console.log(data));
-    console.log('Disponible');
+// function doSomething_async() {
+//     getUserDetails_async((data) => console.log(data));
+//     console.log('Disponible');
+// }
+
+// function getUserDetails_async(callback) {
+//     setTimeout(() => callback('Elise'), 2000);
+// }
+
+// doSomething_async();
+
+const fs = require('fs');
+
+function generateData() {
+    let data = '1';
+    for (i=0; i < 1000000; i++) {
+        data = data + Math.random() * 10;
+    }
+    return data;
 }
 
-function getUserDetails_async(callback) {
-    setTimeout(() => callback('Elise'), 2000);
+function callback() {
+    console.log('Process \'bigfile\' treminé, durée ' + (Date.now() - date) + ' ms.')
 }
 
-doSomething_async();
+fs.writeFile('bigfile', generateData(), callback)
+const date = Date.now();
+console.log('Disponible.');
