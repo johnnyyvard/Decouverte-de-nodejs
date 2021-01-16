62eb83c7d6acebecd6cd5fb7a22b961dcec9ae2d
johnnyyvard <johnnyyvard@me.com>
2021-01-16 07:16:07 +0100

    Gestion opérations bloquantes de façon asynchrone

--- async-io.js
+++ async-io.js
@@ -1,14 +1,26 @@
 // Sync
-function doSomething_sync(){
+function doSomething_sync() {
     let data = getUserDetails_sync();
     console.log(data);
     console.log('Disponible');
 }
 
-function getUserDetails_sync(){
-    const timout =  Date.now() + 2000;
-    while (Date.now() < timout){};
+function getUserDetails_sync() {
+    const timout = Date.now() + 2000;
+    while (Date.now() < timout) { };
     return 'Elise';
 }
 
 doSomething_sync();
+
+// Async
+function doSomething_async() {
+    getUserDetails_async((data) => console.log(data));
+    console.log('Disponible');
+}
+
+function getUserDetails_async(callback) {
+    setTimeout(() => callback('Elise'), 2000);
+}
+
+doSomething_async();
