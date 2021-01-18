fdea7c98c2cec22c3a62ff2fda9676c827717094
johnnyyvard <johnnyyvard@me.com>
2021-01-18 17:19:13 +0100

    Implémentation d'un request handler 2ème partie

--- modules-indispensables/http.js
+++ modules-indispensables/http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const url = require('url');
 
 function handleRequest(req, res) {
-    const parseUrl = new url(req.url);
+    const parseUrl = url.parse(req.url);
     if ('/' === parseUrl.path) {
         res.writeHead(200, '', {'Content-Type': 'text/plain'});
         res.end('Bonjour.');
