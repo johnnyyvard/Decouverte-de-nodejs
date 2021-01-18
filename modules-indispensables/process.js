cf10e49f89d8c5e935c0076f6fb67838b97b7657
johnnyyvard <johnnyyvard@me.com>
2021-01-18 17:28:26 +0100

    Utilisation du module Process

---
+++ modules-indispensables/process.js
@@ -0,0 +1,10 @@
+
+// Environnement
+
+// process.stdin.on(name, callback);
+
+// Std
+process.stdout.write('Bonjour \n', () => console.log('Message affiché'));
+process.stderr.write('Erreur \n', () => console.log('Message affiché'));
+
+// Events
