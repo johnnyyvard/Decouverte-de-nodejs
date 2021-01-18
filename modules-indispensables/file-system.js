9c077b514a005eff8fc4a8e6c1cd8da887379048
johnnyyvard <johnnyyvard@me.com>
2021-01-18 10:45:20 +0100

    Gestion du file system 2ème partie

--- modules-indispensables/file-system.js
+++ modules-indispensables/file-system.js
@@ -1,1 +1,14 @@
 const fs = require('fs');
+const path = '../bigfile';
+
+// Asynchrones
+fs.readFile(path, callback);
+fs.writeFile(path, callback);
+fs.copyFile(path, callback);
+
+// Streams
+fs.createReadStream(path);
+fs.createWriteStream(path);
+
+// Stats
+fs.stat(path);
