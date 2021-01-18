ea875377778603ca51a62f7afe0718f8d5a5fcdd
johnnyyvard <johnnyyvard@me.com>
2021-01-18 20:10:09 +0100

    Appréhension du générateur d'applications

--- app.js
+++ app.js
@@ -12,7 +12,14 @@
 // const exemple = require('./events/limite-callbacks_exemple');
 // exemple.timer(undefined, undefined, () => console.log('3 secondes écoulées.'));
 
-const exemple = require('./events/eventEmitter_exemple');
+// const exemple = require('./events/eventEmitter_exemple');
 
-exemple.timer.on('3_secondes', () => console.log('3 secondes écoulées.'));
-exemple.launchTimer();
+// exemple.timer.on('3_secondes', () => console.log('3 secondes écoulées.'));
+// exemple.launchTimer();
+
+const url = require('url');
+const express = require('express');
+const app = express();
+require('./router').initRouter(app);
+
+app.listen(8000);
