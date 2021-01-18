c6b2b407442694fae8885ad6c51a79a2c8777ccc
johnnyyvard <johnnyyvard@me.com>
2021-01-18 17:36:49 +0100

    Gestion des sorties de process

--- modules-indispensables/process.js
+++ modules-indispensables/process.js
@@ -4,7 +4,16 @@
 // process.stdin.on(name, callback);
 
 // Std
-process.stdout.write('Bonjour \n', () => console.log('Message affiché'));
-process.stderr.write('Erreur \n', () => console.log('Message affiché'));
+// process.stdout.write('Bonjour \n', () => console.log('Message affiché'));
+// process.stderr.write('Erreur \n', () => console.log('Message affiché'));
 
 // Events
+
+function beforeExitCallback(code) {
+    console.log('About to exit with code ' + code);
+    setTimeout( () => console.log('Last async action.'), 1000);
+}
+
+process.on("beforeExit", beforeExitCallback);
+
+console.log('starting');
